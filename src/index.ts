fd898ef26d7263ad40baae8492a7d19bbe4b8a20
William Baierle Charqueiro <william.charqueiro@fockink.ind.br>
2023-06-04 01:03:04 -0300

    fix: index host and package json node version

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import app from './app';
-import { checkConnection } from './database/planetScale.mysql';
+import app from "./app";
+import { checkConnection } from "./database/planetScale.mysql";
 
 const port = process.env.PORT || 8888;
+const host = process.env.HOST || "localhost";
 
 checkConnection();
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}!`);
+  console.log(`Server running on 
+  http://${host}:${port}!`);
 });
